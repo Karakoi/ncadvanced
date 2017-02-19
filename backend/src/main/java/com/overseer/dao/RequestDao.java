908f1be5859499effc1953acb4017f09c21c9d4d
Bohdan Bachkala <redowlave@gmail.com>
2017-02-19 15:58:10 +0200

    Added CRUD Interface

--- backend/src/main/java/com/overseer/dao/RequestDao.java
+++ backend/src/main/java/com/overseer/dao/RequestDao.java
@@ -5,27 +5,5 @@ import com.overseer.model.Request;
 /**
  * The <code>RequestDao</code> interface represents access to Request {@link Request} object in database.
  */
-public interface RequestDao {
-    /**
-     * Create Request object in database.
-     * @param request the request to be inserted
-     * @return      inserted request
-     * @see         Request
-     */
-    Request create(Request request);
-
-    /**
-     * Update Request object in database.
-     * @param request the request to be updated
-     * @see         Request
-     */
-    void update(Request request);
-
-    /**
-     * Delete Request object in database.
-     * @param request the request to be deleted
-     * @see         Request
-     */
-    void delete(Request request);
-
+public interface RequestDao extends CrudDao<Request, Long> {
 }
