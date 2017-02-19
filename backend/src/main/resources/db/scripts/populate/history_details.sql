345afb9afebe8dffe24020430d2fc42f5002c139
Eugene1992 <deyneko55@gmail.com>
2017-02-19 20:02:44 +0200

    #5 Added scripts to populate the test data for 'request',  'history_detail' tables.

---
+++ backend/src/main/resources/db/scripts/populate/history_details.sql
@@ -0,0 +1,3 @@
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-19', 1);
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-20', 1);
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (true, '2016-03-21', 1);
