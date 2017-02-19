50c511c1478c760221f309b88569ea432f6349a7
Eugene1992 <deyneko55@gmail.com>
2017-02-19 22:09:34 +0200

    #5 Added scripts to populate the test data for 'joined_request' table.

--- backend/src/main/resources/db/scripts/populate/history_details.sql
+++ backend/src/main/resources/db/scripts/populate/history_details.sql
@@ -1,3 +1,3 @@
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-19', 1);
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-20', 1);
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (true, '2016-03-21', 1);
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, null, 2);
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-20', 2);
+insert into history_detail (is_actual, date_of_last_change, changer_id) values (true, '2016-03-21', 2);
---
+++ backend/src/main/resources/db/scripts/populate/joined_request.sql
@@ -0,0 +1,1 @@
+insert into joined_request (title, description, assignee_id, history_datail_id, previous_joined_request_id, progress_status_id, priority_status_id) values ('Test joined request', 'Some description', 3, null, null, 1, 2);
