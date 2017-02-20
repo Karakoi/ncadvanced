c749af15b545b365788b839acac7f6f4783ef808
DashaRomanova <Naphanay@3g.ua>
2017-02-20 22:20:14 +0200

    Added interfaces for User

---
+++ backend/src/main/java/com/overseer/service/CrudService.java
@@ -0,0 +1,57 @@
+package com.overseer.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Interface for generic CRUD operations.
+ *
+ * @param <T> entity type.
+ * @param <ID> entity identifier type.
+ */
+public interface CrudService<T, ID extends Serializable> {
+    /**
+     * Saves a given entity.
+     *
+     * @param entity must not be {@literal null}.
+     * @return the saved entity.
+     */
+    T save(T entity);
+
+    /**
+     * Fetches entity by its id.
+     *
+     * @param id must not be {@literal null}.
+     * @return entity with the given id or {@literal null} if none found.
+     */
+    T findOne(ID id);
+
+    /**
+     * Deletes a given entity.
+     *
+     * @param entity must not be {@literal null}.
+     */
+    void delete(T entity);
+
+    /**
+     * Deletes the entity with the given id.
+     *
+     * @param id must not be {@literal null}.
+     */
+    void delete(ID id);
+
+    /**
+     * Checks whether entity with the given id exists.
+     *
+     * @param id must not be {@literal null}.
+     * @return {@literal true} if entity with the given id exists, {@literal false} otherwise.
+     */
+    boolean exists(ID id);
+
+    /**
+     * Returns all entities of the type.
+     *
+     * @return all entities.
+     */
+    List<T> findAll();
+}
--- backend/src/main/java/com/overseer/service/UserService.java
+++ backend/src/main/java/com/overseer/service/UserService.java
@@ -1,7 +1,14 @@
 package com.overseer.service;
 
 /**
- * User service interface.
+ * The <code>UserService</code> interface represents access to UserDao.
  */
-public interface UserService {
+public interface UserService extends CrudService{
+
+    /**
+     * Sends to email new generated password for existed User.
+     * @param email email for recovering password, must not be {@literal null}.
+     */
+    void changePassword(String email);
+
 }
