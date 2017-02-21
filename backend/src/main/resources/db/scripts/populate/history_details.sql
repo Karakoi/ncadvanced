8822c5bff63a7bc4f79863831e8d90cae5f17a70
Eugene1992 <deyneko55@gmail.com>
2017-02-21 17:31:45 +0200

    #5 Updated scripts data based on new ERD structure.

--- backend/src/main/resources/db/scripts/populate/history_details.sql
+++ backend/src/main/resources/db/scripts/populate/history_details.sql
@@ -1,3 +1,12 @@
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, null, 2);
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (false, '2016-03-20', 2);
-insert into history_detail (is_actual, date_of_last_change, changer_id) values (true, '2016-03-21', 2);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'First title', '2017/02/09', 101, 113);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'Second title', '2017/02/10', 101, 113);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'Third title', '2017/02/11', 101, 113);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('description', 'First description', '2017/02/12', 101, 113);
+
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'First title', '2017/02/11', 44, 313);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'Second title', '2017/02/12', 44, 313);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('description', 'First description', '2017/02/13', 44, 313);
+
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'First title', '2017/02/11', 101, 213);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('title', 'Second title', '2017/02/12', 101, 213);
+insert into history (column_name, current_value, date_of_last_change, changer_id, main_row_id) values ('description', 'Second description', '2017/02/12', 101, 213);
--- backend/src/main/resources/db/scripts/populate/joined_request.sql
+++ backend/src/main/resources/db/scripts/populate/joined_request.sql
@@ -1,1 +1,27 @@
-insert into joined_request (title, description, assignee_id, history_datail_id, previous_joined_request_id, progress_status_id, priority_status_id) values ('Test joined request', 'Some description', 3, null, null, 1, 2);
+insert into joined_request (title, description, assignee_id, priority_status_id, progress_status_id) values ('massa quis augue', 'at dolor quis odio consequat', 44, 4, 12);
+insert into joined_request (title, description, assignee_id, priority_status_id, progress_status_id) values ('nulla suspendisse', 'nisl venenatis lacinia aenean sit', 48, 5, 12);
+insert into joined_request (title, description, assignee_id, priority_status_id, progress_status_id) values ('consectetuer adipiscing', 'consequat ut nulla sed', 46, 4, 11);
+insert into joined_request (title, description, assignee_id, priority_status_id, progress_status_id) values ('nulla eget', 'fusce congue diam id ornare imperdiet sapien', 17, 4, 12);
+insert into joined_request (title, description, assignee_id, priority_status_id, progress_status_id) values ('sem mauris laoreet', 'neque sapien placerat ante nulla justo', 51, 6, 8);
+
+UPDATE request SET joined_request_id = 313 WHERE id = 113;
+UPDATE request SET joined_request_id = 313 WHERE id = 114;
+UPDATE request SET joined_request_id = 313 WHERE id = 115;
+
+UPDATE request SET joined_request_id = 314 WHERE id = 116;
+UPDATE request SET joined_request_id = 314 WHERE id = 117;
+UPDATE request SET joined_request_id = 314 WHERE id = 118;
+UPDATE request SET joined_request_id = 314 WHERE id = 119;
+
+UPDATE request SET joined_request_id = 315 WHERE id = 120;
+UPDATE request SET joined_request_id = 315 WHERE id = 121;
+UPDATE request SET joined_request_id = 315 WHERE id = 122;
+UPDATE request SET joined_request_id = 315 WHERE id = 123;
+UPDATE request SET joined_request_id = 315 WHERE id = 124;
+
+UPDATE request SET joined_request_id = 316 WHERE id = 125;
+UPDATE request SET joined_request_id = 316 WHERE id = 126;
+
+UPDATE request SET joined_request_id = 317 WHERE id = 127;
+UPDATE request SET joined_request_id = 317 WHERE id = 128;
+UPDATE request SET joined_request_id = 317 WHERE id = 129;
