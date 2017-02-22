8c2fe1097c0b59fc34ef126fbd0bd7d3559bb4e5
Bohdan Bachkala <redowlave@gmail.com>
2017-02-22 19:14:32 +0200

    #15 Interface DI jdbcOperations & similar code format

--- backend/src/main/java/com/overseer/dao/impl/RequestDaoImpl.java
+++ backend/src/main/java/com/overseer/dao/impl/RequestDaoImpl.java
@@ -4,10 +4,10 @@ import com.overseer.dao.RequestDao;
 import com.overseer.model.PriorityStatus;
 import com.overseer.model.ProgressStatus;
 import com.overseer.model.Request;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -28,110 +28,58 @@ import java.util.Locale;
  */
 @Repository
 @Transactional
-public class RequestDaoImpl implements RequestDao{
+@RequiredArgsConstructor
+public class RequestDaoImpl implements RequestDao {
 
-    private static final String SELECT_REQUEST_BY_PROGRESS_STATUS = "SELECT * FROM request r WHERE r.progress_status =: progressStatus";
-    private static final String SELECT_REQUEST_BY_JOINED_REQUEST_ID = "SELECT * FROM request r WHERE r.joined_request_id =: joinedRequestId";
-    private static final String SELECT_REQUEST_BY_REPORTER_ID = "SELECT * FROM request r WHERE r.reporter_id =: reporterId";
-    private static final String SELECT_REQUEST_BY_ASSIGNEE_ID = "SELECT * FROM request r WHERE r.assignee_id =: assigneeId";
-    private static final String SELECT_REQUEST_BY_PERIOD = "SELECT * FROM request r WHERE r.date_of_creation BETWEEN :begin AND :end";
+    private static final String SELECT_REQUEST_BY_PROGRESS_STATUS =
+            "SELECT * FROM request r WHERE r.progress_status =: progressStatus";
+
+    private static final String SELECT_REQUEST_BY_JOINED_REQUEST_ID =
+            "SELECT * FROM request r WHERE r.joined_request_id =: joinedRequestId";
+
+    private static final String SELECT_REQUEST_BY_REPORTER_ID =
+            "SELECT * FROM request r WHERE r.reporter_id =: reporterId";
+
+    private static final String SELECT_REQUEST_BY_ASSIGNEE_ID =
+            "SELECT * FROM request r WHERE r.assignee_id =: assigneeId";
+
+    private static final String SELECT_REQUEST_BY_PERIOD =
+            "SELECT * FROM request r WHERE r.date_of_creation BETWEEN :begin AND :end";
+
+    private static final String SELECT_REQUEST_BY_PRIORITY_STATUS =
+            "SELECT * FROM request r WHERE r.priority_status =: priorityStatus";
+
     private static final String SELECT_REQUEST_BY_DATE = "SELECT * FROM request r WHERE r.date_of_creation =: date";
-    private static final String SELECT_REQUEST_BY_PRIORITY_STATUS = "SELECT * FROM request r WHERE r.priority_status =: priorityStatus";
+
     private static final String SELECT_REQUEST_BY_ID = "SELECT * FROM request r WHERE r.id = :id";
+
     private static final String SELECT_ALL_REQUESTS = "SELECT * FROM request";
+
     private static final String DELETE_REQUEST_BY_ID = "DELETE FROM request r WHERE r.id = :id";
+
     private static final String UPDATE_REQUEST_BY_ID = "UPDATE request SET"
-                                                            + " title =: title,"
-                                                            + " description = :description,"
-                                                            + " priority_status = :priorityStatus,"
-                                                            + " progress_status = :progressStatus,"
-                                                            + " joined_request_id =: joinedRequestId,"
-                                                            + " reporter_id = :reporterId,"
-                                                            + " assignee_id = :assigneeId,"
-                                                            + " estimate_time_in_days = :estimateTimeInDays,"
-                                                            + " date_of_creation = :dateOfCreation"
-                                                    + " WHERE id = :id";
+            + " title =: title,"
+            + " description = :description,"
+            + " priority_status = :priorityStatus,"
+            + " progress_status = :progressStatus,"
+            + " joined_request_id =: joinedRequestId,"
+            + " reporter_id = :reporterId,"
+            + " assignee_id = :assigneeId,"
+            + " estimate_time_in_days = :estimateTimeInDays,"
+            + " date_of_creation = :dateOfCreation"
+            + " WHERE id = :id";
+
     private static final String INSERT_REQUEST = "INSERT INTO request"
-                                                    + " (title, description, priority_status, progress_status, joined_request_id,"
-                                                    + " reporter_id, assignee_id, estimate_time_in_days, date_of_creation)"
-                                             + " VALUES"
-                                                    + " (:title, :description, :priorityStatus, :progressStatus, :joinedRequestId,"
-                                                    + " :reporterId, :assigneeId, :estimateTimeInDays, :dateOfCreation)";
+            + " (title, description, priority_status, progress_status, joined_request_id,"
+            + " reporter_id, assignee_id, estimate_time_in_days, date_of_creation)"
+            + " VALUES"
+            + " (:title, :description, :priorityStatus, :progressStatus, :joinedRequestId,"
+            + " :reporterId, :assigneeId, :estimateTimeInDays, :dateOfCreation)";
 
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByStatus(ProgressStatus progressStatus) {
-        Assert.notNull(progressStatus);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_PROGRESS_STATUS, new MapSqlParameterSource("progressStatus", progressStatus.name()), new RequestMapper());
-    }
+    private final NamedParameterJdbcOperations jdbc;
 
     /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByJoined(Long joinedRequestId) {
-        Assert.notNull(joinedRequestId);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_JOINED_REQUEST_ID, new MapSqlParameterSource("joinedRequestId", joinedRequestId), new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByAssignee(Long assigneeId) {
-        Assert.notNull(assigneeId);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_ASSIGNEE_ID, new MapSqlParameterSource("assigneeId", assigneeId), new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByReporter(Long reporterId) {
-        Assert.notNull(reporterId);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_REPORTER_ID, new MapSqlParameterSource("reporterId", reporterId), new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByPeriod(LocalDate begin, LocalDate end) {
-        Assert.notNull(begin);
-        Assert.notNull(end);
-        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-        mapSqlParameterSource.addValue("begin", begin,  Types.DATE);
-        mapSqlParameterSource.addValue("end", end,  Types.DATE);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_PERIOD, mapSqlParameterSource, new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestByDate(LocalDate date) {
-        Assert.notNull(date);
-        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-        mapSqlParameterSource.addValue("date", date,  Types.DATE);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_DATE, mapSqlParameterSource, new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
-     */
-    @Override
-    public List<Request> getRequestsByPriority(PriorityStatus priorityStatus) {
-        Assert.notNull(priorityStatus);
-        return namedParameterJdbcTemplate.query(SELECT_REQUEST_BY_PRIORITY_STATUS, new MapSqlParameterSource("priorityStatus", priorityStatus.name()), new RequestMapper());
-    }
-
-    /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public Request save(Request request) {
@@ -150,10 +98,10 @@ public class RequestDaoImpl implements RequestDao{
         if (findOne(request.getId()) != null) {
             id = request.getId();
             mapSqlParameterSource.addValue("id", id);
-            namedParameterJdbcTemplate.update(UPDATE_REQUEST_BY_ID, mapSqlParameterSource);
+            jdbc.update(UPDATE_REQUEST_BY_ID, mapSqlParameterSource);
         } else {
             KeyHolder keyHolder = new GeneratedKeyHolder();
-            namedParameterJdbcTemplate.update(INSERT_REQUEST, mapSqlParameterSource, keyHolder);
+            jdbc.update(INSERT_REQUEST, mapSqlParameterSource, keyHolder);
             id = keyHolder.getKey().longValue();
         }
         request.setId(id);
@@ -161,16 +109,18 @@ public class RequestDaoImpl implements RequestDao{
     }
 
     /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public Request findOne(Long id) {
         Assert.notNull(id);
-        return namedParameterJdbcTemplate.queryForObject(SELECT_REQUEST_BY_ID, new MapSqlParameterSource("id", id), new RequestMapper());
+        return jdbc.queryForObject(SELECT_REQUEST_BY_ID,
+                new MapSqlParameterSource("id", id),
+                new RequestMapper());
     }
 
     /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public void delete(Request request) {
@@ -180,16 +130,17 @@ public class RequestDaoImpl implements RequestDao{
     }
 
     /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public void delete(Long id) {
         Assert.notNull(id);
-        namedParameterJdbcTemplate.update(DELETE_REQUEST_BY_ID, new MapSqlParameterSource("id", id));
+        jdbc.update(DELETE_REQUEST_BY_ID,
+                new MapSqlParameterSource("id", id));
     }
 
     /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public boolean exists(Long id) {
@@ -198,18 +149,103 @@ public class RequestDaoImpl implements RequestDao{
     }
 
     /**
-     * @{inheritDoc}.
+     * {@inheritDoc}.
      */
     @Override
     public List<Request> findAll() {
-        return namedParameterJdbcTemplate.query(SELECT_ALL_REQUESTS, new RequestMapper());
+        return jdbc.query(SELECT_ALL_REQUESTS, new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByStatus(ProgressStatus progressStatus) {
+        Assert.notNull(progressStatus);
+        return jdbc.query(SELECT_REQUEST_BY_PROGRESS_STATUS,
+                new MapSqlParameterSource("progressStatus", progressStatus.name()),
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByJoined(Long joinedRequestId) {
+        Assert.notNull(joinedRequestId);
+        return jdbc.query(SELECT_REQUEST_BY_JOINED_REQUEST_ID,
+                new MapSqlParameterSource("joinedRequestId", joinedRequestId),
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByAssignee(Long assigneeId) {
+        Assert.notNull(assigneeId);
+        return jdbc.query(SELECT_REQUEST_BY_ASSIGNEE_ID,
+                new MapSqlParameterSource("assigneeId", assigneeId),
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByReporter(Long reporterId) {
+        Assert.notNull(reporterId);
+        return jdbc.query(SELECT_REQUEST_BY_REPORTER_ID,
+                new MapSqlParameterSource("reporterId", reporterId),
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByPeriod(LocalDate begin, LocalDate end) {
+        Assert.notNull(begin);
+        Assert.notNull(end);
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("begin", begin, Types.DATE);
+        mapSqlParameterSource.addValue("end", end, Types.DATE);
+        return jdbc.query(SELECT_REQUEST_BY_PERIOD,
+                mapSqlParameterSource,
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestByDate(LocalDate date) {
+        Assert.notNull(date);
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("date", date, Types.DATE);
+        return jdbc.query(SELECT_REQUEST_BY_DATE,
+                mapSqlParameterSource,
+                new RequestMapper());
+    }
+
+    /**
+     * {@inheritDoc}.
+     */
+    @Override
+    public List<Request> getRequestsByPriority(PriorityStatus priorityStatus) {
+        Assert.notNull(priorityStatus);
+        return jdbc.query(SELECT_REQUEST_BY_PRIORITY_STATUS,
+                new MapSqlParameterSource("priorityStatus", priorityStatus.name()),
+                new RequestMapper());
     }
 
     /**
      * The <code>RequestMapper</code> class represents mapper for {@link Request} object.
      */
     private static final class RequestMapper implements RowMapper<Request> {
-
+        /**
+         * Mapping data in {@link ResultSet} to a {@link Request} entity.
+         */
         @Override
         public Request mapRow(ResultSet resultSet, int i) throws SQLException {
             Request request = new Request(resultSet.getDate("date_of_creation").toLocalDate(),
--- backend/src/main/java/com/overseer/dao/impl/SubRequestDaoImpl.java
+++ backend/src/main/java/com/overseer/dao/impl/SubRequestDaoImpl.java
@@ -3,11 +3,11 @@ package com.overseer.dao.impl;
 import com.overseer.dao.SubRequestDao;
 import com.overseer.model.SubRequest;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import java.sql.ResultSet;
@@ -15,21 +15,33 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
- * @{inheritDoc}.
+ * <p>
+ * Implementation of {@link SubRequestDao} interface.
+ * </p>
  */
+@Repository
+@Transactional
 @RequiredArgsConstructor
 public class SubRequestDaoImpl implements SubRequestDao {
-    private static final String SQL_SELECT_ALL = "SELECT * FROM sub_request";
-    private static final String SQL_SELECT_ONE = SQL_SELECT_ALL + "WHERE id = :id";
-    private static final String SQL_SELECT_BY_REQUEST_ID = SQL_SELECT_ALL + "WHERE request_id = :requestId";
-    private static final String SQL_DELETE_ONE = "DELETE FROM sub_request WHERE id = :id";
-    private static final String SQL_EXISTS = "SELECT count(id) FROM sub_request WHERE id = :id";
-    private static final String SQL_SAVE = "INSERT INTO sub_request (id,title,description,request_id"
-            + "VALUES (:id,:title,:description,:request_id))";
 
-    @Autowired
+    private static final String SQL_SELECT_ALL = "SELECT * FROM sub_request";
+
+    private static final String SQL_SELECT_ONE = SQL_SELECT_ALL + "WHERE id = :id";
+
+    private static final String SQL_SELECT_BY_REQUEST_ID = SQL_SELECT_ALL + "WHERE request_id = :requestId";
+
+    private static final String SQL_DELETE_ONE = "DELETE FROM sub_request WHERE id = :id";
+
+    private static final String SQL_EXISTS = "SELECT count(id) FROM sub_request WHERE id = :id";
+
+    private static final String SQL_SAVE =
+            "INSERT INTO sub_request (id,title,description,request_id VALUES (:id,:title,:description,:request_id))";
+
     private final NamedParameterJdbcOperations jdbc;
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public SubRequest save(SubRequest entity) {
         MapSqlParameterSource map = new MapSqlParameterSource("id", entity.getId());
@@ -40,34 +52,52 @@ public class SubRequestDaoImpl implements SubRequestDao {
         return entity;
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public SubRequest findOne(Long id) {
         return jdbc.queryForObject(SQL_SELECT_ONE, new MapSqlParameterSource("id", id),
                 new SubRequestRowMapper());
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public void delete(SubRequest entity) {
         Assert.notNull(entity);
         delete(entity.getId());
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public void delete(Long id) {
         jdbc.update(SQL_DELETE_ONE, new MapSqlParameterSource("id", id));
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public boolean exists(Long id) {
         Integer count = jdbc.queryForObject(SQL_EXISTS, new MapSqlParameterSource("id", id), Integer.class);
         return count != null && count == 1;
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public List<SubRequest> findAll() {
         return jdbc.query(SQL_SELECT_ALL, new SubRequestRowMapper());
     }
 
+    /**
+     * {@inheritDoc}.
+     */
     @Override
     public List<SubRequest> findByRequest(Long requestId) {
         return jdbc.query(SQL_SELECT_BY_REQUEST_ID, new MapSqlParameterSource("requestId", requestId),
@@ -75,12 +105,13 @@ public class SubRequestDaoImpl implements SubRequestDao {
     }
 
     /**
-     * Class witch map result set to entity.
+     * The <code>SubRequestRowMapper</code> class represents mapper for {@link SubRequest} object.
      */
     private static final class SubRequestRowMapper implements RowMapper<SubRequest> {
         /**
-         *Just implementation of spring interface.
+         * Mapping data in {@link ResultSet} to a {@link SubRequest} entity.
          */
+        @Override
         public SubRequest mapRow(ResultSet rs, int rowNum) throws SQLException {
             SubRequest subRequest = new SubRequest();
             subRequest.setId(rs.getLong("id"));
