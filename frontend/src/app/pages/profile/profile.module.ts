028a7e6f797765bc28e3fcaa8ec38ae76bc4f95c
Karakoi <karakoy@yandex.ru>
2017-03-01 20:44:22 +0200

    #36  Updated user profile page

--- frontend/src/app/pages/profile/profile.module.ts
+++ frontend/src/app/pages/profile/profile.module.ts
@@ -1,19 +1,11 @@
+import {Routes, RouterModule} from "@angular/router";
+import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
-import {NgModule} from "@angular/core/src/metadata/ng_module";
-import {Routes, RouterModule} from "@angular/router";
-import {EditProfileComponent} from "./edit-profile/edit-profile.component";
 import {ProfileComponent} from "./profile.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    component: ProfileComponent
-  },
-  {
-    path: 'edit',
-    component: EditProfileComponent
-  }
+  {path: '', component: ProfileComponent},
 ];
 
 @NgModule({
@@ -23,12 +15,10 @@ const routes: Routes = [
     ReactiveFormsModule
   ],
   declarations: [
-    ProfileComponent,
-    EditProfileComponent
+    ProfileComponent
   ],
   exports: [
-    ProfileComponent,
-    EditProfileComponent
+    ProfileComponent
   ]
 })
 export class ProfileModule {
