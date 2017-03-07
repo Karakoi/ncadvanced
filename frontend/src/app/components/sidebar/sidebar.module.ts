1d0aaac7064bcf702f25d269743b96cb9b5adad5
Bohdan Bachkala <redowlave@gmail.com>
2017-03-07 12:31:44 +0200

    mutual styling for pages

--- frontend/src/app/components/sidebar/sidebar.module.ts
+++ frontend/src/app/components/sidebar/sidebar.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
-import {TestModule} from "../../pages/shared/SharedGravatar.module";
+import {GravatarSharedModule} from "../../pages/shared/sharedGravatar.module";
 import {SideBarComponent} from "./sidebar.component";
 
 @NgModule({
@@ -9,7 +9,7 @@ import {SideBarComponent} from "./sidebar.component";
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    TestModule
+    GravatarSharedModule
   ],
   declarations: [
     SideBarComponent
