75d1d85a14cbe39d8a72f695f90fa20f54253571
Maksym <maxi007007@gmail.com>
2017-03-19 17:16:37 +0200

    to change branch

--- frontend/src/app/pages/request-profile/request-profile.component.ts
+++ frontend/src/app/pages/request-profile/request-profile.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {RequestService} from "../../service/request.service";
+import {UserService} from "../../service/user.service";
 import {Request} from "../../model/request.model";
 import {ActivatedRoute} from "@angular/router";
 import {ToastsManager} from "ng2-toastr";
@@ -7,6 +8,7 @@ import {AuthService} from "../../service/auth.service";
 import {User} from "../../model/user.model";
 import {HistoryService} from "../../service/history.service";
 import {History} from "../../model/history.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'request-profile',
@@ -16,14 +18,18 @@ import {History} from "../../model/history.model";
 export class RequestProfileComponent implements OnInit {
 
   currentUser: User;
+  assigneUser: User;
   request: Request;
   showDescription: boolean = true;
   showHistory: boolean = true;
   showSubRequests: boolean = true;
   showJoinedRequests: boolean = true;
   historyRecords: History[];
+  oldValue: string;
+  oldValueAssigneeName: string;
 
   constructor(private requestService: RequestService,
+              private userService: UserService,
               private route: ActivatedRoute,
               private toastr: ToastsManager,
               private authService: AuthService,
@@ -47,6 +53,55 @@ export class RequestProfileComponent implements OnInit {
     this.authService.currentUser.subscribe((user: User) => {
       this.currentUser = user;
     });
+  }
+
+  getOldValue(history: History): void {
+    var result;
+
+    switch (history.columnName) {
+      case "priority_status_id":
+        result = "Ha-ha-ha";
+
+        this.oldValue = result;
+        //return result;
+        break;
+      case "progress_status_id":
+        result = "Ha-ha-ha";
+
+        this.oldValue = result;
+        //return result;
+        break;
+
+      case "assignee_id":
+      this.userService.get(+history.oldValue).subscribe((user: User) => {
+          result = user.firstName;
+          console.log("columnName: " + history.columnName);
+          console.log("Assignee id: " + +history.oldValue);
+          console.log(user);
+          console.log("result from case: " + result);
+
+        this.oldValue = result;
+        //return result;
+        });
+        //result = "WWWWWW";
+      break;
+
+      case "parent_id":
+        result = "Ha-ha-ha";
+
+        this.oldValue = result;
+        //return result;
+        break;
+      default:
+        result = history.oldValue;
+
+        this.oldValue = result;
+        //return result;
+    }
+
+    //result = "QQQQ";
+    /*console.log("return: " + result);
+    return result;*/
   }
 
   changeShowDescription() {
