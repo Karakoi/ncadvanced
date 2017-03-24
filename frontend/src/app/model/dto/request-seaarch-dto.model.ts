d0834db5282dc1212643913ba8904d508b5c394a
Eugene1992 <deyneko55@gmail.com>
2017-03-24 17:10:57 +0200

    #140 Added search events on request table inputs. Created search params dto component.

---
+++ frontend/src/app/model/dto/request-seaarch-dto.model.ts
@@ -0,0 +1,10 @@
+export interface RequestSearchDTO {
+  title: string,
+  dateOfCreation: Date,
+  estimate: number,
+  priority: string,
+  progress: string,
+  reporterName: string,
+  assigneeName: string,
+  limit: number
+}
