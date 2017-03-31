16c02c20fe479435beecece601dffd872e3f7cb9
vasylenko0503 <alkitav@gmail.com>
2017-03-31 15:23:37 +0300

    Fixed manager module router bug

--- frontend/src/app/pages/home/manager/manager.routes.ts
+++ frontend/src/app/pages/home/manager/manager.routes.ts
@@ -1,11 +1,13 @@
 import {ManagerComponent} from "./manager.component";
-import {ManagerPageGuard} from "../../../service/guard/manager-page.guard";
 import {Routes} from "@angular/router";
 
 export const ManagerRoutes: Routes = [
   {
     path: '',
+    component: ManagerComponent
+  },
+  {
+    path: 'manager',
     component: ManagerComponent,
-    canActivate: [ManagerPageGuard]
-  }
+  },
 ];
